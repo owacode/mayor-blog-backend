17e2cd2708b68d59b7ccffe56b4a18b86e9c4e24
mohitpatell <mohit@wednesday.is>
2020-02-17 17:30:50 +0530

    make it better

--- routes/controller/update.js
+++ routes/controller/update.js
@@ -112,7 +112,6 @@ class UpdateController{
         }})
       .then(result => {
         console.log('Updated to NotApprovedAuthor');
-        // AdminMail(values);
         this.updateAuthorProfileMain(values);
         resolve(result);
       })
